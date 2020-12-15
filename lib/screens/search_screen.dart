50fe5457c1afb4526a6f340d365c7224311247cc
irfaandev <irfaandeveloper@outlook.com>
2020-12-15 15:43:38 +0100

    Change: rename model classes (begin with uppercase)

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:outfitted_flutter_mobile/components/outfitted_custom_appbar.dart';
 import 'package:outfitted_flutter_mobile/components/store_category.dart';
 import 'package:outfitted_flutter_mobile/firebase/firebase_config.dart';
-import 'package:outfitted_flutter_mobile/model/category.dart';
+import 'package:outfitted_flutter_mobile/model/Category.dart';
 import 'package:outfitted_flutter_mobile/screens/collection_category_screen.dart';
 import 'package:outfitted_flutter_mobile/style/style.dart';
 
