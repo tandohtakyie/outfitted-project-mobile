a695db21185f2b2e48d6765d9f9e5047486248d9
irfaandev <irfaandeveloper@outlook.com>
2021-01-15 20:28:18 +0100

    @Gibbs I got you bro 💪🏿 --> Change: get and implement highest product price in range

--- lib/components/price_range_slider.dart
+++ lib/components/price_range_slider.dart
@@ -10,32 +10,39 @@ class PriceRangeSlider extends StatefulWidget {
 
 class _PriceRangeSliderState extends State<PriceRangeSlider> {
 
+  static double _lowerValue = 0,
+                _upperValue;
 
+  RangeValues values;
 
-  static double _lowerValue = 1;
-  static double _upperValue = 10;
-
-  RangeValues values = RangeValues(_lowerValue, _upperValue);
-  int price = 0;
+  initMaxPrice() async {
+    await OutFittedApp.firestore
+        .collection(OutFittedApp.collectionProduct)
+        .orderBy('price', descending: true)
+        .limit(1)
+        .get()
+        .then((value) {
+          // print('Highest price GOOD ONE ==> ${value.docs[0].data()['price'].toString()}');
+          setState(() {
+            _upperValue = double.parse(value.docs[0].data()['price'].toString());
+            values = RangeValues(_lowerValue, _upperValue);
+          });
+          // print('PRINT AANGEPAST: $_upperValue');
+        });
+  }
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    getMaxPrice();
-    print('highest price se price price is ==> ' + price.toString());
+    initMaxPrice();
   }
-
 
   @override
   Widget build(BuildContext context) {
-    setState(() {
-      getMaxPrice();
-      print('highest price se gethightest price price is ==> ' + price.toString());
-    });
+    /*print('highest price se BIJ BUILD price price is ==> ' + _upperValue.toString());
+    print(values.start);
+    print(values.end);*/
 
-  print(values.start);
-  print(values.end);
     return SliderTheme(
       data: SliderTheme.of(context).copyWith(
         valueIndicatorColor: kSecondaryColor,
@@ -51,31 +58,11 @@ class _PriceRangeSliderState extends State<PriceRangeSlider> {
         values: values,
         onChanged: (val){
           setState(() {
-            print('values ====> $val');
+            // print('values ====> $val');
             values = val;
           });
         },
       ),
     );
-  }
-  void getMaxPrice() async {
-    await OutFittedApp.firestore
-        .collection(OutFittedApp.collectionProduct)
-        .orderBy('price', descending: true)
-        .get()
-        .then((value) {
-          print('Highest price GOOD ONE ==> ${value.docs[0].data()['price'].toString()}');
-          price = int.parse(value.docs[0].data()['price'].toString());
-          // print('NEEWWWW $price');
-    });
-   // https://stackoverflow.com/questions/50296061/flutter-is-it-possible-to-extract-data-from-a-future-without-using-a-futurebui
-
-    // querySnapshot.docs.forEach((snapshot) {
-    //   tempPrice = snapshot.data()['price'];
-    // });
-    // setState(() {
-    //   price = tempPrice;
-    // });
-
   }
 }
